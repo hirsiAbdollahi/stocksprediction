a763e4f1427ef05e4cb76e0e739c7b61379da48c
hirsiAbdollahi <abdollahi.hirsi@gmail.com>
2020-09-10 11:03:11 +0200

    Class Database

---
+++ database.py
@@ -0,0 +1,18 @@
+import pymongo 
+
+class Database():
+    URI = "mongodb://127.0.0.1:27017"
+
+    def __init__ (self, dbname): 
+        self.client = pymongo.MongoClient(Database.URI)
+        self.dbname = self.client[dbname]
+
+    def insert(self,collection,data):
+        self.dbname[collection].insert(data)
+
+    def find(self,collection, query=None):
+        return self.dbname[collection].find(query)
+
+    def find_one(self,collection, query):
+        return self.dbname[collection].find_one(query)
+
